4cac83f677b365f07302dae2c8ed4310cad88c66
Masljuk Dmitry <dima@aep.mpei.ac.ru>
2007-01-17 00:00:00 +0300

    change update-component

--- commands.ml
+++ commands.ml
@@ -87,8 +87,8 @@ let prepare components =
 let update_component component = (* todo: more smart implementation *)
   with_component_dir ~strict:true component
     (fun () ->
-      git_clean ();
       git_checkout ~force:true ();
+      git_clean ();
       List.iter
       (fun branch ->
 	git_pull ~refspec:branch (Filename.concat (Params.get_param "git-url") component.name))
