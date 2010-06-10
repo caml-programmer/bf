26b897f63fb97c7f8aa0ae9ca4fa4463698de1db
Masljuk Dmitry <dima@caml.ru>
2010-06-10 14:37:48 +0400

    fix load_pluguns

--- rules.ml
+++ rules.ml
@@ -14,8 +14,7 @@ let rules_file () =
   Filename.concat (Sys.getcwd()) ".bf-rules"
 
 let load_plugins () =
-  if debug then
-    print_string "load ac-configure...";
+  if debug then print_string "load ac-configure...";
   Scheme.eval_code (fun _ -> if debug then print_endline "ok") "
 (define-syntax ac-configure
   (syntax-rules ()
@@ -24,10 +23,9 @@ let load_plugins () =
     ((_ e1 e2)       (ml-ac-configure `((e1 ,e2))))
     ((_ (e))         (ml-ac-configure `((e ()))))
     ((_ e)           (ml-ac-configure `((e ()))))
-    ((_)             (ml-ac-configure `()))))
-";
-  if debug then
-    print_string "load make...";
+    ((_)             (ml-ac-configure `()))))";
+
+  if debug then print_string "load make...";
   Scheme.eval_code (fun _ -> if debug then print_endline "ok") "
 (define-syntax make
   (syntax-rules ()
@@ -39,8 +37,7 @@ let load_plugins () =
     ((_ e)               (ml-make `((e ()))))
     ((_)                 (ml-make `()))))";
 
-  if debug then
-    print_string "load export...";
+  if debug then print_string "load export...";
   Scheme.eval_code (fun _ -> if debug then print_endline "ok") "
 (define-syntax export
   (syntax-rules ()
@@ -51,16 +48,14 @@ let load_plugins () =
     ((_ e)           (ml-export `((e ()))))
     ((_)             (ml-export `()))))";
 
-  if debug then
-    print_string "load update-make-params...";
+  if debug then print_string "load update-make-params...";
   Scheme.eval_code (fun _ -> if debug then print_endline "ok") "
 (define-syntax update-make-params
   (syntax-rules ()
     ((_ (e) (e1 e2) ...) (ml-update-make-params `((e ()) (e1 ,e2) ...)))
     ((_  e  (e1 e2) ...) (ml-update-make-params `((e ()) (e1 ,e2) ...)))))";
 
-  if debug then
-    print_string "load user lib.scm...";
+  if debug then print_string "load user lib.scm...";
   let dir = Params.get_param "plugins-dir" in
   let plugdir =
     let start = Params.get_param "start-dir" in
@@ -70,13 +65,13 @@ let load_plugins () =
       Filename.concat start dir
   in
   if not (System.is_regular (Filename.concat plugdir "lib.scm")) then
-    if debug then print_endline "skip"
-  else    
+    (if debug then print_endline "skip")
+  else
     begin
       System.with_extension "scm"
 	Scheme.eval_file
 	(System.with_prefix plugdir (System.list_of_directory plugdir));
-      if debug then print_endline "ok";
+      (if debug then print_endline "ok");
     end
 
 let load_composite file =
