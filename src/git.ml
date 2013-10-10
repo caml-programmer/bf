377dfb8f0148e1353fdd41a95b49bc707aa646e6
Maxim Valyanskiy <maxcom@jet.msk.su>
2013-10-10 18:04:08 +0400

    исправил формат changelog
    теперь указанный в прошлом коммите формат работает

--- src/git.ml
+++ src/git.ml
@@ -71,13 +71,13 @@ let git_log ?(pack=None) ?(diff=false) ?(since=None) tag_a tag_b =
   let cmd =
     match since,diff with
       | Some s,true ->
-	  sprintf "git log --pretty=format:\"%s\" --since='%s' -p %s%s" fmt s tag_a file
+	  sprintf "git log --pretty=\"%s\" --since='%s' -p %s%s" fmt s tag_a file
       | None,true ->
-	  sprintf "git log --pretty=format:\"%s\" -p '%s'..'%s'%s" fmt tag_a tag_b file
+	  sprintf "git log --pretty=\"%s\" -p '%s'..'%s'%s" fmt tag_a tag_b file
       | Some s,false ->
-	  sprintf "git log --pretty=format:\"%s\" --since='%s' %s%s" fmt s tag_a file
+	  sprintf "git log --pretty=\"%s\" --since='%s' %s%s" fmt s tag_a file
       | None,false ->
-	  sprintf "git log --pretty=format:\"%s\" '%s'..'%s'%s" fmt tag_a tag_b file
+	  sprintf "git log --pretty=\"%s\" '%s'..'%s'%s" fmt tag_a tag_b file
   in
   let chunks = ref [] in
   let buf = Buffer.create 64 in
