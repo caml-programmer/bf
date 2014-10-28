be690cce2d6e7793bbdf57679844675d88000717
dima <dima@seal.cpr.jet.msk.su>
2014-10-28 15:17:47 +0300

    Отделяем модуль spectype

--- src/pack.ml
+++ src/pack.ml
@@ -9,58 +9,7 @@ open Ocs_env
 open Ocs_types
 open Platform
 open Printf
-
-type pkg_name = string
-type pkg_desc = string
-type pkg_ver = string
-type pkg_op =
-  | Pkg_le
-  | Pkg_lt
-  | Pkg_eq
-  | Pkg_ge
-  | Pkg_gt
-  | Pkg_last
-    
-type depend = 
-    pkg_name * (pkg_op * pkg_ver) option * pkg_desc option
-
-type reject =
-    string (* pcre pattern *)
-
-type provide =
-    string (* string symbol *)
-
-type spec = {
-  pkgname : pkg_name;
-  depends : depend list;
-  provides : provide list;
-  obsoletes : provide list;
-  rejects : reject list;
-  components : component list;
-  pre_install : string option;
-  pre_update : string option;
-  pre_uninstall : string option;
-  post_install : string option;
-  params : (string,string) Hashtbl.t;
-  hooks : string option;
-}
-
-let string_of_pkg_op = function
-  | Pkg_le -> "<="
-  | Pkg_lt -> "<"
-  | Pkg_eq -> "="
-  | Pkg_ge -> ">="
-  | Pkg_gt -> ">"
-  | Pkg_last -> "="
-
-let string_of_op = function
-  | Pkg_last -> "last"
-  | v -> string_of_pkg_op v
-
-let string_of_pkgexn = function
-  | Commands.Pkg_release_not_found (s,exn) ->
-      sprintf "Pkg_release_not_found(%s,%s)" s (Printexc.to_string exn)
-  | exn -> Printexc.to_string exn
+open Spectype
 
 let home_made_package pkg =
   let exclude =
