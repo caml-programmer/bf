d40c1a93fa18bb77a2fde21ec49d77a26194b915
dima <dima@seal.cpr.jet.msk.su>
2015-02-11 15:54:58 +0300

    Добавляем модуль synonims

---
+++ src/synonims.ml
@@ -0,0 +1,22 @@
+let table = [
+  "Masljuk Dmitry <dima@caml.ru>", [    
+    "Masljuk Dmitry <masljuk@jet.msk.su>";
+    "dima <dima@seal.cpr.jet.msk.su>";
+    "Masljuk Dmitry <dima@aep.mpei.ac.ru>";
+  ];
+  "Vasyukov Evgeniy <vasyukov@jet.msk.su>", [
+    "Vasyukov Yevgeniy <vasyukov@jet.msk.su>";
+    "Yevgeniy Vasyukov <vasyukov@jet.msk.su>";
+    "Yevgeniy Vassyukov <vasyukov@jet.msk.su>";
+    "vasyukov <vasyukov>";
+    "jenya <jenya@jenya.service.jet.msk.su>"
+  ];
+  "Dmitrii Kashin <kashin_ds@jet.msk.su>", [
+    "Dmitrii Kashin <freehck@freehck.ru>"
+  ]
+]
+
+
+let load () =
+  (* todo: load from .bf-synonims *)
+  table
