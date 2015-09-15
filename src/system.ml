1de7803b848fbacf266257ff569eb7722d97ab3a
Dmitrii Kashin <d.kashin@solarsecurity.ru>
2015-09-15 16:00:55 +0300

    BF-35: функции определения количества ядер
    (завязано на linux sysfs)

--- src/system.ml
+++ src/system.ml
@@ -427,3 +427,28 @@ let rec scandir f resource =
     end
   else f resource
 
+let command ?(env=Unix.environment()) ?(ignore_errors=false) ?(filter=(fun _ -> true)) command =
+  let (pout,pin,perr) = Unix.open_process_full command env in
+  let rec read acc ch =
+    try
+      let s = input_line ch in
+      if filter s
+      then read (s::acc) ch
+      else read acc ch
+    with End_of_file -> acc in
+  let outputs = String.concat "\n" (read [] pout) in
+  let errors = String.concat "\n" (read [] perr) in
+  let status = Unix.close_process_full (pout,pin,perr) in
+  match status with
+  | Unix.WEXITED st -> if ignore_errors || st = 0
+		       then (st, outputs, errors)
+		       else failwith (sprintf "Command '%s' exited with non-nil status: %d" command st)
+  | Unix.WSIGNALED signal -> failwith (sprintf "Command '%s' was killed by signal: %d" command signal)
+  | Unix.WSTOPPED signal -> failwith (sprintf "Command '%s' was stopped by signal: %d" command signal)
+
+let cpu_number () =
+  let channel = open_in "/sys/devices/system/cpu/present" in
+  let present_cores = input_line channel in
+  close_in channel;
+  let list_cores = Str.split (Str.regexp "-") present_cores in
+  succ (int_of_string (List.nth list_cores (pred (List.length list_cores))))
